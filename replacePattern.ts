2a74af08e98caaacdae02f0b442d92fc68db337d
Orjan De Smet <orjan.de.smet@gmail.com>
2025-09-03 10:14:54 +0200

    Code Review Remark - Add docstrings

--- replacePattern.ts
+++ replacePattern.ts
@@ -1,15 +1,29 @@
-export function replacePattern(link: string, pattern: RegExp, innerWord: string) {
+/**
+ * Finds all RegExp groups in the innerWord and uses them to replace matching group in the target string with their replacement from the inner word.
+ * The group name {pattern} is kept for backwards compatibility. This group gets replaced with the full innerWord.
+ * @param target The target string to do all replacements in
+ * @param pattern
+ * @param innerWord The keyword from the source text that triggered this replacement
+ * @returns a string based on the target string, in which all occurences of group names or numbers are replaced with their matching group in the innerWord
+ */
+export function replacePattern(target: string, pattern: RegExp, innerWord: string) {
   const groups = findGroups(innerWord, pattern);
 
   const replacements = [["pattern", innerWord], ...groups];
 
   return replacements.reduce((acc, [groupName, replacement]) => {
     return replaceGroup(acc, groupName, replacement);
-  }, link);
+  }, target);
 }
 
-function findGroups(innerWord: string, pattern: RegExp): string[][] {
-  const matches = pattern.exec(innerWord);
+/**
+ * Lists matching RegExp groups in the input string. This includes numbered and named groups.
+ * @param input 
+ * @param pattern 
+ * @returns An array of [groupName, replacement] tuples
+ */
+function findGroups(input: string, pattern: RegExp): string[][] {
+  const matches = pattern.exec(input);
   if (!matches) {
     return [];
   }
@@ -22,6 +36,13 @@ function findGroups(innerWord: string, pattern: RegExp): string[][] {
   return [...numberedGroups, ...namedGroups];
 }
 
+/**
+ * Replaces a substring {groupName} in the input string with the replacement
+ * @param input
+ * @param groupName 
+ * @param replacement 
+ * @returns The string with the replaced values
+ */
 function replaceGroup(input: string, groupName: string, replacement: string): string {
   return input.replace(`{${groupName}}`, replacement);
 }
